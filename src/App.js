125a428b1e7c250610e71c33a35e2a5614dae78f
Suya Kim <kkrladydtn@gmail.com>
2024-03-21 03:40:01 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import { useState } from "react"
 import './App.css';
 import Box from './component/Box';
 
@@ -24,18 +24,53 @@ const choice = {
   },
 };
 function App() {
-
   const [userSelect, setUserSelect] = useState(null)
-  
+  const [computerSelect, setComputerSelect] = useState(null)
+  const [result, setResult] = useState("")
+
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  const judgement = (user, computer) => {
+    // console.log("user", user, "computer", computer)
+
+    // user == computer 비김(tie)
+    // user == "rock", computer == "scissors" user win
+    // user == "rock", computer == "paper" user lose
+    // user == "scissors" computer == "paper" user win
+    // user == "scissors" computer == "paper" user lose 
+    // user == "paper" computer == "rock" user win
+    // user == "paper" computer == "scissors" user lose 
+
+    if(user.name === computer.name){
+      return "tie";
+    } else if (user.name === "Rock") 
+      return computer.name === "Scissors"?"win":"lose";
+    else if (user.name === "Scissors") 
+      return computer.name === "Paper"?"win":"lose";
+    else if (user.name === "Paper") 
+      return computer.name === "Rock"?"win":"lose";
+  };
+
+
+  const randomChoice = () => {
+    // 객체의 키값만 뽑아서 어레이로 만들어주는 함수
+    let itemArray = Object.keys(choice); 
+    // console.log("item array", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final]
   };
 
   return (
     <div>
       <div className="main">
-        <Box title="You" item={userSelect}/>
-        {/* <Box title="Computer"/>   */}
+        <Box title="You" item={userSelect} result ={result}/>
+        <Box title="Computer" item={computerSelect} result ={result}/>  
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
